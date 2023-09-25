22ea8304b2a453946ff70beccd3fe4db133b6675
Tyrese-Paulus <tpwebdevelopment11@gmail.com>
2023-09-25 18:35:45 +0200

    refactoring

--- src/app/components/country-grid/country-grid.component.ts
+++ src/app/components/country-grid/country-grid.component.ts
@@ -11,39 +11,34 @@ import { Subject } from 'rxjs';
 })
 export class CountryGridComponent implements OnInit {
 
-  cities: any = [];
-  selectedCity: any;
+  endsubs$: Subject<any> = new Subject();
+  countries: any = [] ;
 
   p: number = 1;
-  countries: any = [] ;
-  endsubs$: Subject<any> = new Subject();
   visible: boolean = false;
-  countryCode: any;
+
   selectedCountry: any;
-  languages: any;
   selectedLanguages: any;
   selectedCurrencies: any;
-  notDraggable: boolean = false;
   countryCurrencies: any = [];
+
   fifa: boolean = false;
-  currentTime: any;
-  options: any;
   nameValue:any;
   fifaValue: any;
+
+  regions: any = [
+    {name: 'Africa', code: 'africa'},
+    {name: 'Americas', code: 'americas'},
+    {name: 'Europe', code: 'europe'},
+    {name: 'Oceania', code: 'oceania'},
+    {name: 'Asia', code: 'asia'},
+    {name: 'Antarctic', code: 'antarctic'},
+    ];
 
   constructor(private countryService: CountryService) { }
 
   ngOnInit(): void{
     this.getCountries()
-
-    this.cities = [
-      {name: 'Africa', code: 'africa'},
-      {name: 'Americas', code: 'americas'},
-      {name: 'Europe', code: 'europe'},
-      {name: 'Oceania', code: 'oceania'},
-      {name: 'Asia', code: 'asia'},
-      {name: 'Antarctic', code: 'antarctic'},
-    ]
   }
 
   showDialogue(){
@@ -51,12 +46,12 @@ export class CountryGridComponent implements OnInit {
   }
 
   selectCountry(countryCode: any){
+
     this.countryService.getCountry(countryCode).pipe(takeUntil(this.endsubs$)).subscribe((country) => {
 
       this.selectedCountry = country
       this.getLanguages(this.selectedCountry[0].languages)
       this.getCurrencies(this.selectedCountry[0].currencies)
-
 
       if(this.selectedCountry[0].fifa){
         this.fifa = true
@@ -64,11 +59,8 @@ export class CountryGridComponent implements OnInit {
         this.fifa = false
       }
 
-      console.log(this.selectedCountry[0]);
-
-
-
     })
+
   }
 
   getLanguages(languageObj: any){
@@ -76,6 +68,7 @@ export class CountryGridComponent implements OnInit {
   }
 
   getCurrencies(currencyObj: any){
+
     this.selectedCurrencies = Object.values(currencyObj)
 
     this.countryCurrencies = [];
@@ -91,27 +84,31 @@ export class CountryGridComponent implements OnInit {
   }
 
   getCountries(){
+
     this.countryService.getCountries().pipe(takeUntil(this.endsubs$)).subscribe((country) => {
       this.countries = country
     })
+
   }
 
   getRegion(selectedRegion: any){
-    console.log(selectedRegion.value.code);
+
     this.countryService.getCountryRegion(selectedRegion.value.code).pipe(takeUntil(this.endsubs$)).subscribe((regionalCountries) =>{
       this.countries = regionalCountries;
     })
+
   }
 
   getName(){
-    console.log(this.nameValue);
+
     this.countryService.getCountryName(this.nameValue).pipe(takeUntil(this.endsubs$)).subscribe((countryByName) => {
       this.countries = countryByName;
     })
+
   }
 
   getFifa(){
-    console.log(this.fifaValue);
+
     this.countryService.getCountryFifa(this.fifaValue).pipe(takeUntil(this.endsubs$)).subscribe((countryByCode: any) => {
       this.countries = countryByCode
     })
